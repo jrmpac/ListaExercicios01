db6b9d662473fa437e5fc0e8ba5154ad27fc420c
jrmpac <jrmpac>
2023-03-10 15:50:52 -0300

    Adicionada o Exercicio 29

---
+++ ListaExercicio.Exercicio29/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ListaExercicio.Exercicio29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- Tabuada -----");
+            Console.WriteLine();
+
+            for (int num = 0; num <= 10; num++)
+            {
+                Console.WriteLine("Tabuada do " + num + ":");
+
+                for (int i = 0; i <= 10; i++)
+                {
+                    int resultado = num * i;
+                    Console.WriteLine(num + ":" + i + "= " + resultado);
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Pressione qualquer tecla para sair.");
+            Console.ReadLine();
+        }
+    }
+}
